3905cacab5d27087ed7b77d3f1db16a265f026ff
KeisukeSakuma <k.sakuma@ordermade.jp>
2022-11-12 16:37:28 +0900

    ✅ アソシエーションあれこれ

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,11 +1,11 @@
 class SessionsController < ApplicationController
   skip_before_action :login_required
-  
+
   def new
   end
 
   def create
-    user = user.find_by(email: session_params[:email])
+    user = User.find_by(email: session_params[:email])
     if user&.authenticate(session_params[:password])
       session[:user_id] = user.id
       redirect_to root_url, notice: 'ログインしました'
@@ -21,7 +21,7 @@ class SessionsController < ApplicationController
 
   private
 
-  def task_params
-    params.require(:task).permit(:email,:password)
+  def session_params
+    params.require(:session).permit(:email,:password)
   end
 end
